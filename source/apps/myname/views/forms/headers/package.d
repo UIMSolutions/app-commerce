11f19395703da3392e49bde57401abf17e742814
UIMSolutions <ui.manufaktur@gmail.com>
2023-07-12 12:00:48 +0200

    Rename

--- source/apps/myname/views/forms/headers/package.d
+++ source/apps/myname/views/forms/headers/package.d
@@ -1,2 +1,2 @@
-module apps.myname.views.forms.headers;
+module apps.commerce.views.forms.headers;
 
--- source/apps/myname/views/pages/package.d
+++ source/apps/myname/views/pages/package.d
@@ -3,10 +3,10 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module apps.myname.views.pages;
+module apps.commerce.views.pages;
 
 public {
-  import apps.myname.views.pages.headers;
-  import apps.myname.views.pages.bodies;
-  import apps.myname.views.pages.footers;
+  import apps.commerce.views.pages.headers;
+  import apps.commerce.views.pages.bodies;
+  import apps.commerce.views.pages.footers;
 }
